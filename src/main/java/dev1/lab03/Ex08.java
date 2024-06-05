0a2086fa5b0cab0f22ef88b148f419ec159b3ecb
AnamariaTi <112396875+AnamariaTi@users.noreply.github.com>
2024-06-05 18:48:37 +0200

    fini lab 03

--- src/main/java/dev1/lab03/Ex08.java
+++ src/main/java/dev1/lab03/Ex08.java
@@ -1,10 +1,27 @@
-
 package dev1.lab03;
 
 /**
- *exercises tested in main 
+ * exercises tested in main
+ *
  * @author Tirdea Ana-Maria
  */
 public class Ex08 {
-    
+
+    public static void main(String[] args) {
+        System.out.println(9.99 == 10.0);
+        System.out.println(9.99999999999 == 10.0);
+        System.out.println(9.99999999999 == 10.000000001);
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(!true);
+        System.out.println(!false);
+        System.out.println(true && true);
+        System.out.println(true && false);
+        System.out.println(false && true);
+        System.out.println(false && false);
+        System.out.println(true || true);
+        System.out.println(true || false);
+        System.out.println(false || true);
+        System.out.println(false || false);
+    }
 }
