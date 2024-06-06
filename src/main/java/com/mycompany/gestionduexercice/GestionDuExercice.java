0622972cb51f052fdc94d8d78a53abd1845fa279
AnamariaTi <112396875+AnamariaTi@users.noreply.github.com>
2024-06-06 09:21:38 +0200

    lab 04 ex 4 et 5

--- src/main/java/com/mycompany/gestionduexercice/GestionDuExercice.java
+++ src/main/java/com/mycompany/gestionduexercice/GestionDuExercice.java
@@ -9,26 +9,28 @@ import java.util.Scanner;
 public class GestionDuExercice {
 
     public static void main(String[] args) {
-     
-        //asks the user for 10 whole numbers, and for each of these displays, as it goes along, whether it is odd or even.
-        Scanner nomber = new Scanner(System.in); 
-       // int nbValue = 0  ; 
-       for(int i = 0 ; i  < 10 ; i ++){
-            System.out.println("Entrez 10 numero  " + (i+1) + ":" );
-            double no = nomber.nextDouble(); 
-         //    nbValue = nbValue +1 ; 
-            System.out.println("les numero sont : " + no );
-            if(no % 2 == 0){
-                System.out.println("les numero c'est pair sont  : "+ no);
-            }else {
-                System.out.println("les nhumero c'est impair  sont "+  no );
-            }
-         
+
+        //asks the user for n values and displays the sum of these and the average of these values.
+        Scanner moyenne = new Scanner(System.in);
+        Scanner val = new Scanner(System.in);
+        double somme = 0;
+        int nbValeurs = 0;
+        double moy = 0;
+        System.out.println("entre la valeur  de combien nomber voulais  : ");
+        int v = val.nextInt();
+        System.out.println("Entre les " + v + " valeurs : ");
+        while (nbValeurs < v) {
+            double valeur = moyenne.nextDouble();
+            somme = somme + valeur;
+            moy = somme / v;
+            nbValeurs = nbValeurs + 1;
         }
-          
-        
-        
+        System.out.println("le somme de toutes ces valeurs :" + somme);
+        System.out.println("le moyenne de toutes ces valeurs : " + moy);
+
     }
+
+    
 }
 
     
--- src/main/java/dev1/lab04/Ex04.java
+++ src/main/java/dev1/lab04/Ex04.java
@@ -1,10 +1,29 @@
 
 package dev1.lab04;
 
+import java.util.Scanner;
+
 /**
  * *exercises tested in main 
  * @author Tirdea Ana-Maria
  */
 public class Ex04 {
-    
+     public static void main(String[] args) {
+     
+        //asks the user for 10 whole numbers, and for each of these displays, as it goes along, whether it is odd or even.
+        Scanner nomber = new Scanner(System.in); 
+   
+       for(int i = 0 ; i  < 10 ; i ++){
+            System.out.println("Entrez 10 numero  " + (i+1) + ":" );
+            double no = nomber.nextDouble(); 
+       
+            System.out.println("les numero sont : " + no );
+            if(no % 2 == 0){
+                System.out.println("les numero c'est pair sont  : "+ no);
+            }else {
+                System.out.println("les nhumero c'est impair  sont "+  no );
+            }
+         
+        }
+     }
 }
